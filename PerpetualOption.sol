50f3bd8e0feb86d92c1146d9ab4bb8c762e5992f
antimatter <antimatter-finance@pm.me>
2021-04-15 05:35:11 +0800

    require(IERC20(underlying).balanceOf(put) >= totalUnd

--- PerpetualOption.sol
+++ PerpetualOption.sol
@@ -1797,9 +1797,9 @@ contract Factory is Configurable, ContextUpgradeSafe, Constants {
         if(dPut < 0)
             Put(put).burn_(sender, uint(-dPut));
         
-        //require(IERC20(underlying).balanceOf(call) >= totalUnd && IERC20(currency).balanceOf(put) >= totalCur, 'reserve less than expected');
+        //require(IERC20(underlying).balanceOf(put) >= totalUnd && IERC20(currency).balanceOf(call) >= totalCur, 'reserve less than expected');
         //emit Swap(sender, underlying, currency, priceFloor, priceCap, call, put, dCall, dPut, dUnd, dCur);
-        require(IERC20(underlying).balanceOf(call) >= priceFloor && IERC20(currency).balanceOf(put) >= priceCap, 'reserve less than expected');     // share priceFloor and priceCap instead of totalUnd and totalCur to avoid stack too deep errors 
+        require(IERC20(underlying).balanceOf(put) >= priceFloor && IERC20(currency).balanceOf(call) >= priceCap, 'reserve less than expected');     // share priceFloor and priceCap instead of totalUnd and totalCur to avoid stack too deep errors 
         emit Swap(sender, underlying, currency, Call(call).priceFloor(), Call(call).priceCap(), dCall, dPut, dUnd, dCur, data);
     }
     event Swap(address indexed sender, address indexed underlying, address indexed currency, uint priceFloor, uint priceCap, int dCall, int dPut, int dUnd, int dCur, bytes data);
